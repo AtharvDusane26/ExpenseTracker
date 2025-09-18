918f5de72daeccbb09a08c8786b775f312a60610
Shravani <shravanimagdum93@gmail.com>
2025-09-18 23:01:27 +0530

    added expense manager

---
+++ Model/Expenses/ExpenseManager.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseTracker.Model.Expenses
+{
+    internal class ExpenseManager
+    {
+        private User _user;
+        public ExpenseManager(User user)
+        {
+            _user = user;
+        }
+        // to create Expense
+        public void CreateExpense(string name, double amount, DateTime date, string description)
+        {
+            var id = Guid.NewGuid().ToString();
+            Expense expense = new Expense(id);
+            expense.Name = name;
+            expense.Amount = amount;
+            expense.DateOfExpense = date;
+            expense.Descreption = description;
+
+            _user.UserExpenses.Add(expense);
+
+        }
+        //To update Expense
+        public void UpdateExpense(string id, string name, double amount, DateTime date, string description)
+        {
+            var expense = _user.UserExpenses.FirstOrDefault(e => e.ExpenseId == id);
+            if (expense != null)
+            {
+                var updatedExpense = new Expense(id)
+                {
+                    Name = name,
+                    Amount = amount,
+                    DateOfExpense = date,
+                    Descreption = description
+                };
+
+                int index = _user.UserExpenses.IndexOf(expense);
+                _user.UserExpenses[index] = updatedExpense;  // replace old with new
+            }
+        }
+        //to delete Expense
+        public void DeleteExpense(string id)
+        {
+            var expense = _user.UserExpenses.FirstOrDefault(e => e.ExpenseId == id);
+            if (expense != null)
+            {
+                _user.UserExpenses.Remove(expense); // works fine even if property is read-only
+            }
+        }
+
+
+
+
+
+
+    }
+
+}
+    
+
